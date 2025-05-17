9f83235e115fef556022f802a1bef3c94543f882
Aristo Simanjuntak <aristosimanjuntak94@gmail.com>
2025-05-17 13:13:15 +0700

    sosial media update

--- assets/js/script.js
+++ assets/js/script.js
@@ -147,7 +147,7 @@ for (let i = 0; i < navigationLinks.length; i++) {
     for (let i = 0; i < pages.length; i++) {
       if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
         pages[i].classList.add("active");
-        navigationLinks[i].classList.add("active");
+        // navigationLinks[i].classList.add("active");
         window.scrollTo(0, 0);
       } else {
         pages[i].classList.remove("active");
